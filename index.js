9f2da7b4800723968441469b1cdf1436bba1bd15
Justin Sacbibit <jsacbibit@gmail.com>
2014-08-31 21:37:38 -0400

    Remove unnecessary dep (ejs)

--- index.js
+++ index.js
@@ -4,18 +4,12 @@ var express    = require('express')
   , mongoose   = require('mongoose')
   , passport   = require('passport')
   , nodemailer = require('nodemailer')
-  , ejs        = require('ejs')
   , path       = require('path')
   , api        = require('./api/app.js');
 
 var port = process.env.PORT || 8080;
 
-// Render the homepage
-app.engine('html', ejs.renderFile);
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res) {
-  res.render('index.html');
-});
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // REST API
 api.use(app, mongoose, passport, nodemailer);
