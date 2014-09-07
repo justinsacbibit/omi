359407b8289d82cd29e8b5eb9a008b026fc62781
Justin Sacbibit <jsacbibit@gmail.com>
2014-09-07 10:53:02 -0400

    Implement GET ower requests endpoint

--- api/controllers/requests.js
+++ api/controllers/requests.js
@@ -1,7 +1,25 @@
-var error = require('../utils/error.js');
+var OwerRequestModel = require('../models/requests/owerRequest.js')
+  , error            = require('../utils/error.js');
 
 var allOwerRequests = function(req, res) {
-  return error.notImplemented(res);
+  var facebookId = req.user.facebookId
+    , type       = req.query.type;
+
+  if (type) {
+    if (type === 'received') {
+      conditions.to = facebookId;
+    } else if (type === 'sent') {
+      conditions.from = facebookId;
+    } else {
+      return error.badRequest(res, 'Invalid parameter: type must be either "received" or "sent"');
+    }
+  }
+
+  OwerRequestModel.findAsync(conditions)
+  .then(function(owerRequests) {
+    res.json(owerRequests);
+  })
+  .catch(error.serverHandler(res));
 };
 
 exports.owers = {
