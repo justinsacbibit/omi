617bc9befab8639b9fba8ecfce6aed71bc39f504
Justin Sacbibit <jsacbibit@gmail.com>
2014-10-19 01:48:37 -0400

    Implement controller logic for local tran

--- app/controllers/local-transactions.server.controller.js
+++ app/controllers/local-transactions.server.controller.js
@@ -4,51 +4,98 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
+    LocalTransaction = mongoose.model('LocalTransaction'),
+    User = mongoose.model('User'),
+    Ower = mongoose.model('Ower'),
+    errorHandler = require('./errors'),
     _ = require('lodash');
 
 /**
  * Create a Local transaction
  */
 exports.create = function(req, res) {
+  req.body.ower = req.ower.id;
+  req.user = req.user.id;
+  LocalTransaction.newLocalTransaction(req.body, req.ower, function(err, localTransaction) {
+    if (err) return errorHandler.badRequest(res, err);
+    if (!localTransaction) return errorHandler.server(res, 'Unable to create local transaction');
 
+    res.status(201).json(localTransaction);
+  });
 };
 
 /**
  * Show the current Local transaction
  */
 exports.read = function(req, res) {
-
+  res.json(req.localTransaction);
 };
 
 /**
  * Update a Local transaction
  */
 exports.update = function(req, res) {
+  req.localTransaction.update(req.body, req.ower, function(err, localTransaction) {
+    if (err) return errorHandler.badRequest(res, err);
 
+    res.json(localTransaction);
+  });
 };
 
 /**
  * Delete an Local transaction
  */
 exports.delete = function(req, res) {
+  req.localTransaction.delete(req.ower, function(err, localTransaction) {
+    if (err) return errorHandler.server(res, err);
 
+    res.json({
+      message: 'Successfully deleted local transaction'
+    });
+  });
 };
 
 /**
  * List of Local transactions
  */
 exports.list = function(req, res) {
+  LocalTransaction.find({
+    user: req.user.id
+  }, function(err, localTransactions) {
+    if (err) return errorHandler.server(res, err);
 
+    res.json(localTransactions);
+  });
 };
 
 exports.hasAuthorization = function hasAuthorization(req, res, next) {
+  if (String(req.localTransaction.user) !== String(req.user.id)) {
+    return errorHandler.forbidden(res, 'User is not authorized to perform that action');
+  }
+  next();
+};
 
+exports.attachOwer = function attachOwer(req, res, next) {
+  Ower.findById(req.body.owerId).exec(function(err, ower) {
+    if (err) return next(err);
+    if (!ower) return next(new Error('Ower not found'));
+    req.ower = ower;
+    next();
+  });
 };
 
 exports.canCreate = function canCreate(req, res, next) {
-
+  if (String(req.ower.tetheredTo) !== String(req.user.id)) {
+    return errorHandler.forbidden(res, 'User is not authorized to create a transaction for that ower');
+  }
+  next();
 };
 
 exports.localTransactionById = function localTransactionById(req, res, next, id) {
-
+  LocalTransaction.findById(id).exec(function(err, localTransaction) {
+    if (err) return next(err);
+    if (!localTransaction) return next(new Error('Local transaction not found'));
+    req.localTransaction = localTransaction;
+    next();
+  });
 };
--- app/routes/local-transactions.server.routes.js
+++ app/routes/local-transactions.server.routes.js
@@ -6,12 +6,12 @@ module.exports = function(app) {
 
   app.route('/api/v1/local-transactions')
     .get(users.requiresToken, localTransactions.list)
-    .post(users.requiresToken, localTransactions.canCreate, localTransactions.create);
+    .post(users.requiresToken, localTransactions.attachOwer, localTransactions.canCreate, localTransactions.create);
 
   app.route('/api/v1/local-transactions/:localTransactionId')
     .get(users.requiresToken, localTransactions.hasAuthorization, localTransactions.read)
-    .put(users.requiresToken, localTransactions.hasAuthorization, localTransactions.update)
-    .delete(users.requiresToken, localTransactions.hasAuthorization, localTransactions.delete);
+    .put(users.requiresToken, localTransactions.attachOwer, localTransactions.hasAuthorization, localTransactions.update)
+    .delete(users.requiresToken, localTransactions.attachOwer, localTransactions.hasAuthorization, localTransactions.delete);
 
   app.param('localTransactionId', localTransactions.localTransactionById);
 };
