545713852d65595619216a5848d9dfc393163bbc
Justin Sacbibit <jsacbibit@gmail.com>
2015-03-05 20:29:18 -0500

    Convert tabs to spaces

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,47 +1,47 @@
 'use strict';
 
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
-	function($scope, $http, $location, Authentication) {
-		$scope.authentication = Authentication;
+  function($scope, $http, $location, Authentication) {
+    $scope.authentication = Authentication;
 
-		// If user is signed in then redirect back home
-		if ($scope.authentication.user) $location.path('/');
+    // If user is signed in then redirect back home
+    if ($scope.authentication.user) $location.path('/');
 
-		$scope.signup = function() {
-			$http.post('/auth/signup', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
+    $scope.signup = function() {
+      $http.post('/auth/signup', $scope.credentials).success(function(response) {
+        // If successful we assign the response to the global user model
+        $scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				if (response.message.errors) {
-					var reduce = function(collection, reducer, memo) {
-						for (var key in collection) {
-							memo = reducer(memo, collection[key], key);
-						}
-						return memo;
-					};
+        // And redirect to the index page
+        $location.path('/');
+      }).error(function(response) {
+        if (response.message.errors) {
+          var reduce = function(collection, reducer, memo) {
+            for (var key in collection) {
+              memo = reducer(memo, collection[key], key);
+            }
+            return memo;
+          };
 
-					$scope.error = reduce(response.message.errors, function(memo, value, key) {
-						return memo + value.message + '.\n';
-					}, '');
-				} else {
-					$scope.error = response.message;
-				}
-			});
-		};
+          $scope.error = reduce(response.message.errors, function(memo, value, key) {
+            return memo + value.message + '.\n';
+          }, '');
+        } else {
+          $scope.error = response.message;
+        }
+      });
+    };
 
-		$scope.signin = function() {
-			$http.post('/auth/signin', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
+    $scope.signin = function() {
+      $http.post('/auth/signin', $scope.credentials).success(function(response) {
+        // If successful we assign the response to the global user model
+        $scope.authentication.user = response;
 
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
-		};
-	}
+        // And redirect to the index page
+        $location.path('/');
+      }).error(function(response) {
+        $scope.error = response.message;
+      });
+    };
+  }
 ]);
